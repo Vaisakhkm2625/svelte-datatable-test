da3ca0fef6559df240ad1cd45bd0d8f1bb47ca6a
Vaisakh K M <vaisakhkm2625@gmail.com>
2024-05-25 09:04:12 +0530

    datalist

--- src/App.svelte
+++ src/App.svelte
@@ -1,47 +1,46 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+    import { DataHandler } from "@vincjo/datatables";
+    //import data from "$site/data/data";
+    import data from "./lib/people";
+    import Th from "./Th.svelte";
+
+    const handler = new DataHandler(data, { rowsPerPage: 100 });
+    const rows = handler.getRows();
+    const rowcount = handler.getRowCount();
 </script>
 
-<main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
 
-  <div class="card">
-    <Counter />
-  </div>
+{$rowcount.total}
 
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
-</main>
+<table>
+    <thead>
+        <tr>
+            <Th {handler} orderBy="first_name">First name</Th>
+            <Th {handler}>Last name</Th>
+            <Th {handler}>Email</Th>
+        </tr>
+    </thead>
+    <tbody>
+        {#each $rows as row}
+            <tr>
+                <td>{row.first_name}</td>
+                <td>{row.last_name}</td>
+                <td>{row.phone_number}</td>
+            </tr>
+        {/each}
+    </tbody>
+</table>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
+    table {
+        text-align: left;
+        border-collapse: separate;
+        border-spacing: 0;
+        width: 100%;
+    }
+    td,
+    th {
+        padding: 4px 20px;
+        border-bottom: 1px solid #eee;
+    }
 </style>
