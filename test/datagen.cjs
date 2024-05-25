e216c1c145ca77dd4ff3e500ebbb859f74cb7762
Vaisakh K M <vaisakhkm2625@gmail.com>
2024-05-25 09:08:00 +0530

    faker testing script added
    node test/datagen.cjs will generate list to people.json in lib

---
+++ test/datagen.cjs
@@ -0,0 +1,44 @@
+const { faker } = require('@faker-js/faker');
+const fs = require('fs');
+
+function generatePeople(num) {
+    const people = [];
+
+    for (let i = 0; i < num; i++) {
+        const person = {
+            id: i + 1,
+            first_name: faker.person.firstName(),
+            last_name: faker.person.lastName(),
+            phone_number: faker.phone.number(),
+            location: {
+                city: faker.location.city(),
+                state: faker.location.state(),
+                country: faker.location.country()
+            },
+            group_name: faker.company.name()
+        };
+        people.push(person);
+    }
+
+    return people;
+}
+
+const numPeople = 20000;
+const people = generatePeople(numPeople);
+
+// Convert the data to the specified format
+const formattedData = people.map((person, index) => ({
+    id: person.id,
+    first_name: `'${person.first_name}'`,
+    last_name: `'${person.last_name}'`,
+    phone_number: `'${person.phone_number}'`,
+    location: person.location,
+    group_name: `'${person.group_name}'`
+}));
+
+// Export the data
+const exportData = `export default ${JSON.stringify(formattedData, null, 4)}`;
+
+fs.writeFileSync('src/lib/people.js', exportData);
+
+console.log(`Generated ${numPeople} people and saved to people.js`);
